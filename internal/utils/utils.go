68b779179dd8b4e04d6828d3ea175f31eda57c1a
chibx <111882496+chibx@users.noreply.github.com>
2026-05-13 08:19:04 +0100

    Add utility function for clearing multiple cookies

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,13 @@
 package utils
 
+import "github.com/gofiber/fiber/v3"
+
 func AsPointer[T any](a T) *T {
 	return &a
 }
 
+func ClearCookies(ctx fiber.Ctx, cookies ...string) {
+	for _, v := range cookies {
+		ctx.ClearCookie(v)
+	}
+}
