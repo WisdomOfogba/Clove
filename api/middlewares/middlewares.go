6687dda26b78e64e171c0c812e3cdc22bc0b847b
chibx <111882496+chibx@users.noreply.github.com>
2026-05-13 08:50:10 +0100

    Add a deviceId middleware

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -1,1 +1,39 @@
 package middlewares
+
+import (
+	"time"
+
+	"github.com/chibx/vendor-pulse/internal/constants"
+	"github.com/chibx/vendor-pulse/internal/global"
+	"github.com/chibx/vendor-pulse/internal/types/response"
+	"github.com/gofiber/fiber/v3"
+	"github.com/google/uuid"
+)
+
+var logger = global.Logger
+
+func EnsureDeviceIDIsSet(ctx fiber.Ctx) error {
+	deviceId := ctx.Cookies(constants.DeviceIDKey)
+	deviceUUID, err := uuid.Parse(deviceId)
+	if err != nil {
+		deviceUUID, err = uuid.NewRandom()
+		if err != nil {
+			global.Logger.Error().Err(err).Msg("Error generating deviceUUID")
+			return response.FromFiberError(ctx, fiber.ErrInternalServerError)
+		}
+
+		deviceId = deviceUUID.String()
+
+		// Set the deviceId anyways
+		ctx.Cookie(&fiber.Cookie{
+			Name:     constants.DeviceIDKey,
+			Value:    deviceId,
+			Expires:  time.Now().Add(constants.DeviceIDDur),
+			SameSite: "Strict",
+			HTTPOnly: true,
+			Secure:   true,
+		})
+	}
+	ctx.Locals(constants.DeviceIDKey, deviceUUID)
+	return ctx.Next()
+}
