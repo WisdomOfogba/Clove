35bce4a760ca41b148c9e6a9f2a0f11f331cc5af
chibx <111882496+chibx@users.noreply.github.com>
2026-05-13 08:50:16 +0100

    New Commit

--- api/routes/setup.go
+++ api/routes/setup.go
@@ -1,17 +1,18 @@
 package routes
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/chibx/vendor-pulse/api/middlewares"
+	"github.com/gofiber/fiber/v3"
+)
 
 func AddRoutes(app *fiber.App) {
-	customerAuth := app.Group("/auth/customer")
-	customerAuth.Post("/signup", CustomerRegister())
-	customerAuth.Post("/login", CustomerLogin())
-	customerAuth.Post("/logout", CustomerLogout())
+	api := app.Group("/api", middlewares.EnsureDeviceIDIsSet)
+	customerAuth := api.Group("/auth/customer")
+	customerAuth.Post("/signup", UserRegister())
+	customerAuth.Post("/login", UserLogin())
+	customerAuth.Post("/logout", UserLogout())
+	customerAuth.Post("/promote-account", UpgradeToBusinessAccount())
 	
-	sellerAuth := app.Group("/auth/seller")
-	sellerAuth.Post("/signup", CustomerRegister())
-	sellerAuth.Post("/login", CustomerLogin())
-	sellerAuth.Post("/logout", CustomerLogout())
 
 	app.Get("/", func(ctx fiber.Ctx) error {
 		return ctx.JSON(fiber.Map{
