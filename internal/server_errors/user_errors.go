6b1a9bf1917ac2e4ecbc61e461379263c2aa3794
chibx <111882496+chibx@users.noreply.github.com>
2026-05-13 08:20:17 +0100

    Add more descriptive errors

---
+++ internal/server_errors/user_errors.go
@@ -0,0 +1,52 @@
+package server
+
+type (
+	SessionErrType int
+	UserErrType    int
+)
+
+const (
+	SessionExpired SessionErrType = iota
+	SessionDiffIpAddr
+	SessionInvalidIpAddr
+	SessionDiffUserAgent
+)
+
+type TokenErr struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e *TokenErr) Error() string {
+	return e.Message
+}
+
+func NewTokenErr(code int, message string) *TokenErr {
+	return &TokenErr{Code: code, Message: message}
+}
+
+type SessionErr struct {
+	Type    SessionErrType
+	Message string
+}
+
+func (e *SessionErr) Error() string {
+	return e.Message
+}
+
+func NewSessionErr(errType SessionErrType, message string) *SessionErr {
+	return &SessionErr{Type: errType, Message: message}
+}
+
+type UserErr struct {
+	Type    UserErrType
+	Message string
+}
+
+func (e *UserErr) Error() string {
+	return e.Message
+}
+
+func NewUserErr(errType UserErrType, message string) *UserErr {
+	return &UserErr{Type: errType, Message: message}
+}
